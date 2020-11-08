caca3dd5a89d7adf33b19b841757028c21da3f8b
Iván (profe) <ivan@adalab.es>
2020-11-08 22:13:34 +0100

    Transform App (functional component) to class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      Content goes here
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Content goes here
+      </div>
+    );
+  }
 }
 
 export default App;
